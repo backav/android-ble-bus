280f9aef89e0ffa2dccb9ce119a7e6a0257bd277
Bac <wo@xiangyang.li>
2016-05-25 16:38:28 +0800

    增加Samples

---
+++ samples/src/main/java/li/xiangyang/android/blebus_samples/HeartRateActivity.java
@@ -0,0 +1,97 @@
+package li.xiangyang.android.blebus_samples;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+import li.xiangyang.android.blebus.BleBus;
+import li.xiangyang.android.blebus.BleService;
+import li.xiangyang.android.blebus.IBusListener;
+
+public class HeartRateActivity extends Activity {
+    private final static UUID UUID_SERVICE_HEARTRATE = UUID
+            .fromString("0000180D-0000-1000-8000-00805f9b34fb");// 心率服务
+    private final static UUID UUID_C_HEARTRATE = UUID
+            .fromString("00002A37-0000-1000-8000-00805F9B34FB");// 心率特征
+
+    private TextView txtHr;
+
+    private String mDeviceAddress;
+    private BleBus mBus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart_rate);
+
+        txtHr = (TextView) findViewById(R.id.txtHeartrate);
+
+        Intent in = getIntent();
+        mDeviceAddress = in.getStringExtra("address");
+
+        mBus = new BleBus(this, new IBusListener() {
+            @Override
+            public void deviceConnected(String address) {
+                toast("设备连接成功");
+            }
+
+            @Override
+            public void deviceDisconnected(String address) {
+                toast("设备断开连接");
+            }
+
+            @Override
+            public void openBluetoothFailed() {
+                toast("请打开蓝牙");
+            }
+
+            @Override
+            public void deviceRssiRead(String address, boolean success, int rssi) {
+
+            }
+
+            @Override
+            public void listenOperateResult(BleService service, UUID characteristic, boolean success) {
+
+            }
+
+            @Override
+            public void writeOperateResult(BleService service, UUID characteristic, boolean success) {
+
+            }
+
+            @Override
+            public void readOperateResult(BleService service, UUID characteristic, boolean success) {
+
+            }
+
+            @Override
+            public void dataReceived(BleService service, UUID characteristic, final byte[] data) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txtHr.setText("" + data[1]);
+                    }
+                });
+            }
+        });
+
+        mBus.listen(new BleService(mDeviceAddress, UUID_SERVICE_HEARTRATE, UUID_C_HEARTRATE, BleService.OperateType.Notify));
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        mBus.stop();
+        super.onDestroy();
+    }
+
+    private void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ samples/src/main/java/li/xiangyang/android/blebus_samples/MainActivity.java
@@ -0,0 +1,141 @@
+package li.xiangyang.android.blebus_samples;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import li.xiangyang.android.midialog.OptionsDialog;
+
+public class MainActivity extends Activity implements CommonListAdapter.ViewFormater<MainActivity.BleDevice> {
+
+    private ListView mListView;
+
+    private List<BleDevice> mDevices = new ArrayList<>();
+    private CommonListAdapter<BleDevice> mAdapter;
+    private Map<String, Integer> mDeviceRssi = new HashMap<>();
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
+                new OptionsDialog(MainActivity.this, new OptionsDialog.IListener() {
+                    @Override
+                    public void onCancel() {
+                    }
+
+                    @Override
+                    public void onSelected(int index, String option) {
+                        if (index == 0) {
+                            Intent in = new Intent(MainActivity.this, HeartRateActivity.class);
+                            in.putExtra("address", mDevices.get(i).address);
+                            startActivity(in);
+                        }
+                    }
+                }, "设备类型", "心率").show();
+            }
+        });
+
+        findViewById(R.id.btnScan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startScan();
+            }
+        });
+
+        mAdapter = new CommonListAdapter<BleDevice>(this, mDevices, null, R.layout.item_device, 0, this);
+        mListView.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        mBluetoothAdapter.stopLeScan(callback);
+
+        super.onDestroy();
+    }
+
+    private void startScan() {
+
+        if (mBluetoothAdapter.isEnabled()) {
+            mBluetoothAdapter.stopLeScan(callback);
+            mBluetoothAdapter.startLeScan(callback);
+        } else {
+            Toast.makeText(this, "请打开蓝牙", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    BluetoothAdapter.LeScanCallback callback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice bluetoothDevice, final int i, byte[] bytes) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (!mDeviceRssi.containsKey(bluetoothDevice.getAddress())) {
+                        mDevices.add(new BleDevice(bluetoothDevice.getName(), bluetoothDevice.getAddress()));
+                    }
+                    mDeviceRssi.put(bluetoothDevice.getAddress(), i);
+                    mAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+    };
+
+    @Override
+    public void formatItemView(BleDevice item, View view, int index, boolean group) {
+        ViewHolder vh = (ViewHolder) view.getTag();
+        if (vh == null) {
+            vh = new ViewHolder((TextView) view.findViewById(R.id.txtName), (TextView) view.findViewById(R.id.txtRssi));
+            view.setTag(vh);
+        }
+
+        vh.txtName.setText(item.name + "(" + item.address + ")");
+        vh.txtRssi.setText(mDeviceRssi.get(item.address) + "");
+    }
+
+    class BleDevice {
+        String name;
+        String address;
+
+        public BleDevice(String name, String address) {
+            this.name = name;
+            this.address = address;
+        }
+    }
+
+    class ViewHolder {
+        TextView txtName;
+        TextView txtRssi;
+
+        public ViewHolder(TextView txtName, TextView txtRssi) {
+            this.txtName = txtName;
+            this.txtRssi = txtRssi;
+        }
+    }
+}
